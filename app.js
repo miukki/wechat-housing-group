423827b2cdb8a6f44292e1c9b6548d40b0666d28
Anja Ishmukhametova <miukki@gmail.com>
2016-09-26 03:47:49 +0800

    fixes for wechat-housing group

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var express = require('express');
 var wechat = require('wechat');
 var List = wechat.List;
@@ -5,11 +7,11 @@ var cookieParser = require('cookie-parser');
 var expressSession = require('express-session');
 
 List.add('view', [
-  ['reply {a}', function (info, req, res, next) {
-    res.reply('Im Answer A');
-    res.reply('Welcome to app! I am bot, will send you hints. Command-lists: \n 1. if you are looking for apartment (seeker), use command-line,: /s:your_description_about_what_a_you_looking_for  \n 2. if you wanna find tenant, please use command-line: /a:your_description. \n 3. If you wanna add images, please send first command-line /a:imgs  ,  and then add images. \n 4. If everything well done, dont forget send last command-line /push .');
+  ['::help:: Welcome to app! I am bot! ', function (info, req, res, next) {
 
-  }],
+  }]
+
+  /*
   ['reply {b}', function (info, req, res, next) {
     res.reply('Im Answer B');
     res.reply('Welcome to app! I am bot, will send you hints. Command-lists: \n 1. if you are looking for apartment (seeker), use command-line,: /s:your_description_about_what_a_you_looking_for  \n 2. if you wanna find tenant, please use command-line: /a:your_description. \n 3. If you wanna add images, please send first command-line /a:imgs  ,  and then add images. \n 4. If everything well done, dont forget send last command-line /push .');
@@ -18,6 +20,8 @@ List.add('view', [
     res.reply('Im Answer C');
     res.reply('Welcome to app! I am bot, will send you hints. Command-lists: \n 1. if you are looking for apartment (seeker), use command-line,: /s:your_description_about_what_a_you_looking_for  \n 2. if you wanna find tenant, please use command-line: /a:your_description. \n 3. If you wanna add images, please send first command-line /a:imgs  ,  and then add images. \n 4. If everything well done, dont forget send last command-line /push .');
   }]
+
+  */
 ]);
 
 var app = express();
@@ -30,61 +34,107 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+/*
 app.use('/wechat', wechat('token', wechat.text(function (info, req, res, next) {
+
+  console.log('info, req, res, next', info, req, res, next);
+
   if (info.Content === 'list') {
-    res.wait('view'); // view is the very waiter we setuped before.
+    res.wait('view'); // view is the very wait we setuped before.
   } else {
     res.reply('hehe');
     // or stop the waiter and quit.
     // res.nowait('hehe');
   }
 })));
+*/
+var ad = {};
+var _key;
 
-/*
+app.use('/wechat', wechat('token').text(function (message, req, res, next) {
 
-app.use('/wechat',   wechat('token').text(function (message, req, res, next) {
-  // TODO
+  console.log('message', message);
+
+  var isKey = function(k){
+    return _key === k;
+  };
+
+  var addKey = function(k){
+    if (k === '/d' || k === '/l' || k === '/img' || k === '/help' || k === '/push' ) {
+      return k;
+    }
+    return _key;
+  };
+
+  var isContent = function (content) {
+    return content !== '/l' && content !== '/d' && content !== '/img' && content !== '/help' && content !== '/push' && content;
+  };
 
   // message is located in req.weixin
   // four  typoe of messages http://mp.weixin.qq.com/wiki/home/index.html
-  console.log('message.Content' ,message.Content);
 
-  if (message.Content === 'list') {
-    res.wait('view'); // view is the very waiter we setuped before.
+  //access the hint with list of keys
+  if (message.Content === '/help') {
+    res.wait('view'); // list of commands.
   } else {
-    console.log('1111')
     //res.reply('hehe');
-    // res.nowait('hehe');
+    res.nowait('You progress: ', JSON.stringify(ad));
   }
 
 
-  console.log('text', message);
+  _key = addKey(message.Content);
+  console.log('_key', _key);
 
-    // seeker
-    if (message.Content === '/s:'){
+
+  if (!_key) {
+    console.log('_key is undefined !');
+    return;
+  }
+
+  // /d
+  if (isKey('/d')){
+    if (isContent(message.Content)) {
+
+      ad.desc = ad.desc || [];
+      ad.desc.push(message.Content);
+      console.log('ad.desc', ad.desc);
 
     }
+  }
 
-    //aprt
-    if (message.Content === '/a:') {
+  // /l
+  if (isKey('/l')) {
+    if (isContent(message.Content)) {
+
+      ad.location = ad.location || [];
+      ad.location.push(message.Content);
+      console.log('ad.location', ad.location);
 
     }
+  }
 
-    //aprt:images
-    if (message.Content === '/a:imgs') {
 
+  if (isKey('/img')) {
+    if (isContent(message.Content)) {
     }
+  }
 
-    //done
-    if (message.Content === '/push') {
-    //push request to server
+  // /img
+  if (isKey('/push')) {
 
-      res.reply('This adds will be pushed onto the server!');
-    }
+    console.log('post', ad);
+    res.reply('This adds will be pushed into the server: ', ad);
+
+  }
+
 
 
 }).image(function (message, req, res, next) {
-  console.log('image', message);
+ 
+  ad.img = ad.img || [];
+  ad.img.push(message.PicUrl);
+  console.log('ad.img', ad.img);
+
   // TODO
 }).voice(function (message, req, res, next) {
   console.log('voice', message);
@@ -106,8 +156,6 @@ app.use('/wechat',   wechat('token').text(function (message, req, res, next) {
 
 
 
-*/
-
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
@@ -118,6 +166,6 @@ var server = app.listen(3000, function () {
 
 app.use(function(err, req, res, next) {
   console.error('err', err, err.stack);
-  res.status(500).send('Something broke!');
+  res.status(500).send('500 Error!');
 });
 
--- config.js
+++ config.js
@@ -2,6 +2,25 @@
 get your test public-app here
 http://mp.weixin.qq.com/debug/cgi-bin/sandboxinfo?action=showinfo&t=sandbox/index
 */
+
+
+/*
+documentation in chinese
+http://mp.weixin.qq.com/wiki/10/0234e39a2025342c17a7d23595c6b40a.html
+
+english version 
+http://admin.wechat.com/wiki/index.php?title=Create
+http://admin.wechat.com/wiki/index.php?title=Main_Page
+
+public account login page:
+mp.weixin.qq.com
+
+documentation https://github.com/falcondai/wechat-nodejs-guide
+sendbox for wechat public account http://mp.weixin.qq.com/debug/cgi-bin/sandbox?t=sandbox/login
+
+
+
+*/
 {
     "appid": "wxd9313ccf772afee6",//your app id
     "appsecret": "9910c0ef08efdb4d6815a9c26bac4b56"//your appsecret
